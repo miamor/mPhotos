2b45cdef68ec423b8bf95a52e7f12df52f6fa93d
tunguyen <tunguyenhsgs@gmail.com>
2019-04-14 20:01:10 +0700

    14.04.19

---
+++ data/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Database configuration
+$dbHost = 'localhost'; // Host of the database
+$dbUser = 'root'; // Username of the database
+$dbPassword = ''; // Password of the database
+$dbName = 'photos_manager'; // Database name
+$dbTablePrefix = ''; // Table prefix
+
+?>
